1f8eb50a4e0d5c52d3eec722e586bb909375f4e5
bcmfranco <bruno@codev.com.ar>
2024-02-12 23:24:13 -0300

    add blank

---
+++ src/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="container">
+    <h1>
+      Virgin Vue
+    </h1>
+
+    <p class="message">{{ message }}</p>
+    <button @click="changeMessage">Cambiar Mensaje</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Este es un mensaje dentro del componente."
+    };
+  },
+  methods: {
+    changeMessage() {
+      this.message = "Nuevo mensaje cambiado por el botón.";
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+*{
+  font-family: 'Roboto', sans-serif;
+}
+
+#container{
+  text-align: center;
+}
+
+#container .message {
+  color: red;
+}
+
+@media only screen and (max-width: 480px) {
+  /* Tus estilos específicos para dispositivos móviles aquí */
+  #container {
+    widows: 400px;
+    margin: 0 auto;
+  }
+}
+
+</style>
